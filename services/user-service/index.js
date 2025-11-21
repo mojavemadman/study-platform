3372811f46570467628b695bbeb5f7a9c9c0dfaf
mojavemadman <mojavemadmangit@gmail.com>
2025-11-21 02:03:48 -0800

    create user service entry point and db config

---
+++ services/user-service/index.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+import pool from "./config/database.js";
+import express from "express";
+import morgan from "morgan";
+import usersRouter from "./routes/users-routes.js"
+
+const app = express();
+
+app.use(express.json())
+app.use(morgan("dev"));
+app.use("/users", usersRouter);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, async () => {
+    console.log(`User service running on http://localhost:${PORT}`);
+
+    //Test database connection
+    try {
+        await pool.query("SELECT NOW()");
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection failed:", console.error);
+    }
+});
