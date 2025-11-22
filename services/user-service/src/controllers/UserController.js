66035ed2d66e430e0a7c7198172b1b27fd24cb29
mojavemadman <mojavemadmangit@gmail.com>
2025-11-22 13:23:54 -0800

    create login feature

--- services/user-service/src/controllers/UserController.js
+++ services/user-service/src/controllers/UserController.js
@@ -28,8 +28,6 @@ class UserController {
                 return res.status(400).send("Email already registered");
             }
 
-            if (userData.displayName === undefined) userData.displayName = `firstName + lastName`; 
-
             const newUser = await User.createUser(userData);
             delete newUser.password;
 
@@ -39,4 +37,43 @@ class UserController {
             res.status(500).send({ error: "Internal server error" })
         }
     }
+
+    static async loginUser(req, res) {
+        try {
+            const { email, password } = req.body;
+            const user = await User.findUserByEmail(email);
+
+            //Check login credentials
+            if (!user) {
+                return res.status(404).send({ error: "User not found" });
+            }
+
+            const hashedPassword = user.password;
+            const isMatch = await bcrypt.compare(password, hashedPassword);
+
+            if (!isMatch) {
+                return res.status(401).send({ error: "Incorrect login credentials" })
+            }
+
+            //If credentials match, update last_login and issue JWT
+            await User.updateLastLogin(user.id);
+
+            const payload = {
+                userId: user.id,
+                email: user.email,
+            };
+
+            const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+                expiresIn: process.env.JWT_EXPIRY
+            });
+            delete user.password;
+            res.status(200).send({ user, token });
+        } catch (error) {
+            console.error("Error logging in:", error);
+            res.status(500).send({ error: "Internal server error" });
+        }
+    }
+
+    
+
 }
--- services/user-service/src/models/User.js
+++ services/user-service/src/models/User.js
@@ -5,7 +5,7 @@ class User {
     static async createUser(userData) {
         const query = `
             INSERT INTO users (email, password, display_name, first_name, last_name)
-            values ($1, $2, $3, $4, $5)
+            VALUES ($1, $2, $3, $4, $5)
             RETURNING *
         `;
         const hashedPassword = await bcrypt.hash(userData.password, 10);
@@ -22,6 +22,17 @@ class User {
     static async findUserByEmail(email) {
         const query = `SELECT * FROM users WHERE email = $1`;
         const result = await pool.query(query, [email]);
+        return result.rows[0];
+    }
+
+    static async updateLastLogin(userId) {
+        const query = `
+            UPDATE users
+            SET last_login = CURRENT_TIMESTAMP
+            WHERE id = $1
+            RETURNING *
+        `;
+        const result = await pool.query(query, [userId]);
         return result.rows[0];
     }
 }
